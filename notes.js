0cbcdaa3b33aaa10499e219a830cbb3013382967
peter fotis <pfotis@gmail.com>
2021-06-29 21:33:39 +1000

    store the note to json file

--- notes.js
+++ notes.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
+const { Console } = require('console');
 
 const addNotes = function (title, body) {
     const notes = loadNotes()
-
-    notes.push({
-        title: title,
-        body: body
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
     })
-    saveNotes(notes)
+    if (duplicateNotes.length === 0 ) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
 }
 
 const saveNotes = function (notes) {
